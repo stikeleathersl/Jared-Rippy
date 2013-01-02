0e3f3a4d625ea3c2442b72ab338be6843cd5fe10
Rachel Rippy <rachel@rachelrippy.com>
2013-01-02 14:06:29 -0700

    re-order taxonomy pages

--- jared_notes/loop-taxonomy-month.php
+++ jared_notes/loop-taxonomy-month.php
@@ -8,8 +8,45 @@
 
 
 	<?php 
-	// The basic loop
+	// Old posts on top - query by taxonomy term
+	if ( has_term('january', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'january', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('february', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'february', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('march', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'march', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('april', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'april', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('may', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'may', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('june', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'june', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('july', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'july', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('august', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'august', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('september', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'september', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('october', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'october', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('november', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'november', 'order' => 'ASC' ) );
+	}
+	else if ( has_term('december', 'month')) {
+		query_posts( array( 'post_type' => 'daily-bear', 'month' => 'december', 'order' => 'ASC' ) );
+	}
 	
+	// the basic loop
 	while (have_posts() ) : the_post();
 
 	// Use this hook to do things between above the post
@@ -24,14 +61,14 @@
             <div class="bear_image"><?php the_content(); ?></div>
             
         </div>
- 
-        
+         
 	 <?php // Use this hook to do things below the post
 	 notesblog_below_post(); ?>
 	
 	<?php // End the loop
-	
-	endwhile; ?>
+	endwhile;
+    wp_reset_query(); ?>
     
+  
 
 </div>
